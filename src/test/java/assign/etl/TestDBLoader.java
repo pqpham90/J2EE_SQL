a86be31cb053ea13be175e88f951c21e24be93f7
Phillip Pham <pqpham90@gmail.com>
2015-04-26 20:11:35 -0500

    Needa do functional tests

--- src/test/java/assign/etl/TestDBLoader.java
+++ src/test/java/assign/etl/TestDBLoader.java
@@ -15,44 +15,44 @@ public class TestDBLoader extends TestCase {
 		etlHandler = new DBLoader();
 	}
 	
-//	@Test
-//	public void testMeetingInsert() {
-//		try {
-//			String teamMeetingName = "Hyrule";
-//			String year = "1997";
-//			String meetingName = "Triforce";
-//			String link = "Link";
-//
-//			int assignmentId = etlHandler.addMeeting(teamMeetingName, year, meetingName, link);
-//			System.out.println("Meeting ID:" + assignmentId);
-//
-//			Meeting proxyServer = etlHandler.getMeeting(teamMeetingName);
-//			assertEquals(proxyServer.getTeamMeetingName(), teamMeetingName);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	@Test
-//	public void testMeetingtGetWithId() {
-//		try {
-//			String teamMeetingName = "Balamb";
-//			String year = "1999";
-//			String meetingName = "Gunblade";
-//			String link = "Squall";
-//
-//			int assignmentId = etlHandler.addMeeting(teamMeetingName, year, meetingName, link);
-//			System.out.println("Meeting ID:" + assignmentId);
-//
-//			Meeting proxyServer = etlHandler.getMeeting(assignmentId);
-//			assertEquals(proxyServer.getTeamMeetingName(), teamMeetingName);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	@Test
+	public void testMeetingInsert() {
+		try {
+			String teamMeetingName = "Hyrule";
+			String year = "1997";
+			String meetingName = "Triforce";
+			String link = "Link";
+
+			int assignmentId = etlHandler.addMeeting(teamMeetingName, year, meetingName, link);
+			System.out.println("Meeting ID:" + assignmentId);
+
+			Boolean meetingCreated = etlHandler.meetingExists(teamMeetingName);
+			assertTrue(meetingCreated);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
-	public void testGetMeetingWithName() {
+	public void testMeetingtGetWithId() {
+		try {
+			String teamMeetingName = "Balamb";
+			String year = "1999";
+			String meetingName = "Gunblade";
+			String link = "Squall";
+
+			int assignmentId = etlHandler.addMeeting(teamMeetingName, year, meetingName, link);
+			System.out.println("Meeting ID:" + assignmentId);
+
+			Meeting proxyServer = etlHandler.getMeeting(assignmentId);
+			assertEquals(proxyServer.getTeamMeetingName(), teamMeetingName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetMeetingWithName1() {
 		try {
 			String meetingName = "solum_team_meetingz";
 			Boolean meetingExists = etlHandler.meetingExists(meetingName);
@@ -64,12 +64,36 @@ public class TestDBLoader extends TestCase {
 	}
 
 	@Test
-	public void testGetMeetingWithYear() {
+	public void testGetMeetingWithName2() {
+		try {
+			String meetingName = "solum_team_meeting";
+			Boolean meetingExists = etlHandler.meetingExists(meetingName);
+
+			assertTrue(meetingExists);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetMeetingWithYear1() {
 		try {
 			String year = "1980";
 			Boolean yearExists = etlHandler.yearExists(year);
 
 			assertFalse(yearExists);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetMeetingWithYear2() {
+		try {
+			String year = "2014";
+			Boolean yearExists = etlHandler.yearExists(year);
+
+			assertTrue(yearExists);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
